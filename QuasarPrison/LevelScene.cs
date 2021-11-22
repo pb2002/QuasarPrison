1c283ce19a03c407f960ec04c30bff29b60c01f8
Pepijn Bakker <bakker.pepijn@gmail.com>
2021-11-22 12:30:02 +0100

    added level scene

---
+++ QuasarPrison/LevelScene.cs
@@ -0,0 +1,12 @@
+﻿using MonoNode;
+
+namespace QuasarPrison
+{
+    public class LevelScene : RootNode
+    {
+        public LevelScene()
+        {
+            // initialize your scene here
+        }
+    }
+}
